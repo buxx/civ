bee49308f993d722566b6bcd9ed36c2227ab3249
Bastien Sevajol <contact@bux.fr>
2025-01-25 23:39:34 +0100

    feat: wui egui integration

--- crates/civ_wui/src/menu/mod.rs
+++ crates/civ_wui/src/menu/mod.rs
@@ -1,6 +1,10 @@
+pub mod gui;
 use bevy::prelude::*;
+use bevy_egui::{egui, EguiContexts, EguiPlugin};
+
 use camera::spawn_camera;
 use despawn::despawn_menu;
+use gui::manage_gui;
 use manage::manage_menu;
 use spawn::spawn_menu;
 
@@ -18,9 +22,13 @@ pub struct Menu;
 
 impl Plugin for MenuPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, spawn_camera)
+        app.add_plugins(EguiPlugin)
+            .add_systems(Startup, spawn_camera)
             .add_systems(OnEnter(AppState::Menu), spawn_menu)
-            .add_systems(Update, manage_menu.run_if(in_state(AppState::Menu)))
+            .add_systems(
+                Update,
+                (manage_menu, manage_gui).run_if(in_state(AppState::Menu)),
+            )
             .add_systems(OnExit(AppState::Menu), despawn_menu);
     }
 }
