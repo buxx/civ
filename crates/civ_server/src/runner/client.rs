af2e44da10481846fd18f3938619f793198db362
Bastien Sevajol <contact@bux.fr>
2025-07-11 23:01:03 +0200

    chore: split runner client code

--- crates/civ_server/src/runner/client.rs
+++ crates/civ_server/src/runner/client.rs
@@ -31,111 +31,148 @@ impl Runner {
         client: &Client,
         message: ClientToServerMessage,
     ) -> Result<Vec<Effect>, RunnerError> {
-        let state = self.state();
         match message {
-            ClientToServerMessage::Network(message) => match &message {
-                ClientToServerNetworkMessage::Hello(client, resolution) => {
-                    let state = self.state();
-                    let server_resume = state.server_resume(self.context.context.rules());
-                    let player_flag = state
-                        .clients()
-                        .player_state(client.player_id())
-                        .map(|s| s.flag())
-                        .cloned();
-                    let mut shines = vec![(
-                        ServerToClientMessage::Establishment(
-                            ServerToClientEstablishmentMessage::ServerResume(
-                                server_resume,
-                                player_flag,
-                            ),
-                        ),
-                        vec![*client.client_id()],
-                    )];
-                    if let Some(window) = state
-                        .clients()
-                        .states()
-                        .get(client.player_id())
-                        .map(|state| Window::from_around(&state.window().center(), resolution))
-                    {
-                        shines.extend(vec![
-                            (
-                                ServerToClientMessage::InGame(ServerToClientInGameMessage::State(
-                                    ClientStateMessage::SetWindow(window),
-                                )),
-                                vec![*client.client_id()],
-                            ),
-                            (
-                                ServerToClientMessage::InGame(ServerToClientInGameMessage::State(
-                                    ClientStateMessage::SetGameFrame(*self.state().frame()),
-                                )),
-                                vec![*client.client_id()],
-                            ),
-                        ]);
-                        // FIXME BS NOW: c'est le bazar entre take place et hello !
-                        let game_slice = Extractor::new(
-                            self.context.state(),
-                            self.context
-                                .world
-                                .read()
-                                .expect("Consider world as always readable"),
-                        )
-                        .game_slice(client, &window);
+            ClientToServerMessage::Network(message) => self.client_network(client, message),
+            ClientToServerMessage::Game(message) => self.client_game(client, message),
+        }
+    }
 
-                        shines.push((
-                            ServerToClientMessage::InGame(ServerToClientInGameMessage::State(
-                                ClientStateMessage::SetGameSlice(game_slice),
-                            )),
-                            vec![*client.client_id()],
-                        ));
-                    }
+    fn client_network(
+        &self,
+        _client: &Client,
+        message: ClientToServerNetworkMessage,
+    ) -> Result<Vec<Effect>, RunnerError> {
+        match &message {
+            ClientToServerNetworkMessage::Hello(client, resolution) => {
+                self.client_hello(client, resolution)
+            }
+            ClientToServerNetworkMessage::Goodbye => Ok(vec![]),
+        }
+    }
 
-                    Ok(vec![
-                        Effect::State(StateEffect::Clients(ClientsEffect::Insert(
-                            *client.client_id(),
-                            *client.player_id(),
-                        ))),
-                        Effect::Shines(shines),
-                    ])
-                }
-                ClientToServerNetworkMessage::Goodbye => Ok(vec![]),
-            },
-            ClientToServerMessage::Game(message) => {
-                match message {
-                    ClientToServerGameMessage::Establishment(message) => match message {
-                        ClientToServerEstablishmentMessage::TakePlace(flag, resolution) => {
-                            self.player_take_place(client, &flag, resolution)
-                        }
-                    },
-                    ClientToServerGameMessage::InGame(message) => {
-                        let flag = state.client_flag(client)?;
-                        if !Access::new(&self.context).can(flag, &message) {
-                            return Err(RunnerError::DealClientRequest(
-                                DealClientRequestError::Unauthorized,
-                            ));
-                        };
+    fn client_hello(
+        &self,
+        client: &Client,
+        resolution: &Resolution,
+    ) -> Result<Vec<Effect>, RunnerError> {
+        let state = self.state();
+        let server_resume = state.server_resume(self.context.context.rules());
+        let player_flag = state
+            .clients()
+            .player_state(client.player_id())
+            .map(|s| s.flag())
+            .cloned();
+        let mut shines = vec![(
+            ServerToClientMessage::Establishment(ServerToClientEstablishmentMessage::ServerResume(
+                server_resume,
+                player_flag,
+            )),
+            vec![*client.client_id()],
+        )];
+        if let Some(window) = state
+            .clients()
+            .states()
+            .get(client.player_id())
+            .map(|state| Window::from_around(&state.window().center(), resolution))
+        {
+            shines.extend(vec![
+                (
+                    ServerToClientMessage::InGame(ServerToClientInGameMessage::State(
+                        ClientStateMessage::SetWindow(window),
+                    )),
+                    vec![*client.client_id()],
+                ),
+                (
+                    ServerToClientMessage::InGame(ServerToClientInGameMessage::State(
+                        ClientStateMessage::SetGameFrame(*self.state().frame()),
+                    )),
+                    vec![*client.client_id()],
+                ),
+            ]);
+            // FIXME BS NOW: c'est le bazar entre take place et hello !
+            let game_slice = Extractor::new(
+                self.context.state(),
+                self.context
+                    .world
+                    .read()
+                    .expect("Consider world as always readable"),
+            )
+            .game_slice(client, &window);
 
-                        match message {
-                            ClientToServerInGameMessage::SetWindow(window) => {
-                                Ok(vec![Effect::Action(Action::UpdateClientWindow(
-                                    *client, window,
-                                ))])
-                            }
-                            ClientToServerInGameMessage::Unit(unit_id, message) => {
-                                //
-                                self.refresh_unit_on(&unit_id, message)
-                            }
-                            ClientToServerInGameMessage::City(city_id, message) => {
-                                //
-                                self.refresh_city_on(&city_id, message)
-                            }
-                        }
-                    }
-                }
+            shines.push((
+                ServerToClientMessage::InGame(ServerToClientInGameMessage::State(
+                    ClientStateMessage::SetGameSlice(game_slice),
+                )),
+                vec![*client.client_id()],
+            ));
+        }
+
+        Ok(vec![
+            Effect::State(StateEffect::Clients(ClientsEffect::Insert(
+                *client.client_id(),
+                *client.player_id(),
+            ))),
+            Effect::Shines(shines),
+        ])
+    }
+
+    fn client_game(
+        &self,
+        client: &Client,
+        message: ClientToServerGameMessage,
+    ) -> Result<Vec<Effect>, RunnerError> {
+        match message {
+            ClientToServerGameMessage::Establishment(message) => {
+                self.client_establishment(client, message)
+            }
+            ClientToServerGameMessage::InGame(message) => self.client_ingame(client, message),
+        }
+    }
+
+    fn client_establishment(
+        &self,
+        client: &Client,
+        message: ClientToServerEstablishmentMessage,
+    ) -> Result<Vec<Effect>, RunnerError> {
+        match message {
+            ClientToServerEstablishmentMessage::TakePlace(flag, resolution) => {
+                self.client_take_place(client, &flag, resolution)
             }
         }
     }
 
-    pub fn player_take_place(
+    fn client_ingame(
+        &self,
+        client: &Client,
+        message: ClientToServerInGameMessage,
+    ) -> Result<Vec<Effect>, RunnerError> {
+        let state = self.state();
+        let flag = state.client_flag(client)?;
+        if !Access::new(&self.context).can(flag, &message) {
+            return Err(RunnerError::DealClientRequest(
+                DealClientRequestError::Unauthorized,
+            ));
+        };
+
+        match message {
+            ClientToServerInGameMessage::SetWindow(window) => {
+                //
+                Ok(vec![Effect::Action(Action::UpdateClientWindow(
+                    *client, window,
+                ))])
+            }
+            ClientToServerInGameMessage::Unit(unit_id, message) => {
+                //
+                self.refresh_unit_on(&unit_id, message)
+            }
+            ClientToServerInGameMessage::City(city_id, message) => {
+                //
+                self.refresh_city_on(&city_id, message)
+            }
+        }
+    }
+
+    fn client_take_place(
         &self,
         client: &Client,
         flag: &Flag,
