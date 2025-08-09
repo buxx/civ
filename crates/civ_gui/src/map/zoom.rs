73b377bec0934f77e232b401b005dcbe8f95c62b
Bastien Sevajol <contact@bux.fr>
2025-08-09 16:19:57 +0200

    feat: zoom by step

--- crates/civ_gui/src/map/zoom.rs
+++ crates/civ_gui/src/map/zoom.rs
@@ -1,34 +1,47 @@
-use bevy::{
-    input::mouse::{MouseScrollUnit, MouseWheel},
-    prelude::*,
-};
+use bevy::{input::mouse::MouseWheel, prelude::*};
+
+enum ZoomType {
+    ZoomIn,
+    ZoomOut,
+}
+
+impl From<f32> for ZoomType {
+    fn from(value: f32) -> Self {
+        if value.is_sign_negative() {
+            return Self::ZoomOut;
+        }
+
+        Self::ZoomIn
+    }
+}
+
+impl ZoomType {
+    fn new_scale(&self, scale: f32) -> f32 {
+        match self {
+            ZoomType::ZoomIn => match scale {
+                4.0 => 2.0,
+                _ => 1.0,
+            },
+            ZoomType::ZoomOut => match scale {
+                1.0 => 2.0,
+                _ => 4.0,
+            },
+        }
+    }
+}
 
 pub fn map_zoom(
     mut camera: Query<&mut Transform, With<Camera2d>>,
-    mut evr_scroll: EventReader<MouseWheel>,
+    mut event: EventReader<MouseWheel>,
 ) {
-    for ev in evr_scroll.read() {
-        match ev.unit {
-            MouseScrollUnit::Line => {
-                let mut scale = camera.single().scale;
-                scale.x += ev.y;
-                scale.y += ev.y;
-                scale.x = scale.x.max(1.0);
-                scale.y = scale.y.max(1.0);
-                scale.x = scale.x.round();
-                scale.y = scale.y.round();
-                camera.single_mut().scale = scale;
-            }
-            MouseScrollUnit::Pixel => {
-                let mut scale = camera.single().scale;
-                scale.x += ev.y / 100.;
-                scale.y += ev.y / 100.;
-                scale.x = scale.x.max(1.0);
-                scale.y = scale.y.max(1.0);
-                scale.x = scale.x.round();
-                scale.y = scale.y.round();
-                camera.single_mut().scale = scale;
-            }
-        }
+    for ev in event.read() {
+        let mut scale = camera.single().scale;
+        let zoom_type: ZoomType = ev.y.into();
+
+        let new_scale = zoom_type.new_scale(scale.y);
+        scale.x = new_scale;
+        scale.y = new_scale;
+
+        camera.single_mut().scale = scale;
     }
 }
