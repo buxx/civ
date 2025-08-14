06f13c2352a7562201e094072ec54988c01ef649
Bastien Sevajol <contact@bux.fr>
2025-08-14 13:50:53 +0200

    tests: benches

--- crates/civ_server/benches/tasks.rs
+++ crates/civ_server/benches/tasks.rs
@@ -42,7 +42,7 @@ fn runner(context: Context, state: Arc<RwLock<State>>) -> Runner {
         .build()
 }
 
-fn runner_with_fibonacci_tasks(tasks_count: usize, complexity: u64, iterations: usize) {
+fn build_runner(tasks_count: usize, complexity: u64) -> Runner {
     let context = Context::new(Box::new(Std1RuleSet), ServerConfig::default());
     let mut state = State::empty(D2Size::new(1, 1));
     for _ in 0..tasks_count {
@@ -57,16 +57,32 @@ fn runner_with_fibonacci_tasks(tasks_count: usize, complexity: u64, iterations: 
     }
 
     let state = Arc::new(RwLock::new(state));
-    let mut runner = runner(context.clone(), Arc::clone(&state));
-    runner.workers_channels = setup_workers(&runner.context);
+    let world = WorldReader::new(PathBuf::new(), 0, 0, vec![]);
+    let (_, from_clients_receiver) = unbounded::<(Client, ClientToServerMessage)>();
+    let (to_clients_sender, _) = unbounded::<(ClientId, ServerToClientMessage)>();
+    let runner_context = RunnerContext::new(
+        context,
+        state,
+        Arc::new(RwLock::new(world)),
+        from_clients_receiver,
+        to_clients_sender,
+    );
 
+    let mut runner = Runner::builder()
+        .context(runner_context)
+        .tick_base_period(1_000_000_000) // To ensure no wait before ticks
+        .build();
+    runner.workers_channels = setup_workers(&runner.context);
+    runner
+}
+
+fn run(runner: &mut Runner, tasks_count: usize, iterations: usize) {
     for _ in 0..iterations {
         runner.do_one_iteration();
-        assert!(!context.stop_is_required());
     }
 
     let expected_testing_value = (tasks_count * iterations) as u64;
-    let testing_value = state.read().unwrap().testing();
+    let testing_value = runner.context.state().testing();
     assert_eq!(testing_value, expected_testing_value);
 }
 
@@ -82,43 +98,60 @@ pub fn criterion_benchmark(c: &mut Criterion) {
 
     // runner_with_fibonacci_tasks
     c.bench_function("runner_with_fibonacci_tasks 8🎯 1🧠 1k➰", |b| {
-        b.iter(|| runner_with_fibonacci_tasks(black_box(8), black_box(1), black_box(1000)))
+        b.iter_with_setup(
+            || build_runner(8, 1),
+            |mut runner| run(black_box(&mut runner), black_box(8), black_box(1_000)),
+        )
     });
     c.bench_function("runner_with_fibonacci_tasks 1k🎯 1🧠 1k➰", |b| {
-        b.iter(|| runner_with_fibonacci_tasks(black_box(1000), black_box(1), black_box(1000)))
+        b.iter_with_setup(
+            || build_runner(1_000, 1),
+            |mut runner| run(black_box(&mut runner), black_box(1_000), black_box(1_000)),
+        )
     });
     c.bench_function("runner_with_fibonacci_tasks 10k🎯 1🧠 1k➰", |b| {
-        b.iter(|| runner_with_fibonacci_tasks(black_box(10000), black_box(1), black_box(1000)))
+        b.iter_with_setup(
+            || build_runner(10_000, 1),
+            |mut runner| run(black_box(&mut runner), black_box(10_000), black_box(1_000)),
+        )
     });
 
     c.bench_function("runner_with_fibonacci_tasks 8🎯 1M🧠 1k➰", |b| {
-        b.iter(|| runner_with_fibonacci_tasks(black_box(8), black_box(1_000_000), black_box(1000)))
+        b.iter_with_setup(
+            || build_runner(8, 1_000_000),
+            |mut runner| run(black_box(&mut runner), black_box(8), black_box(1_000)),
+        )
     });
     c.bench_function("runner_with_fibonacci_tasks 1k🎯 1M🧠 1k➰", |b| {
-        b.iter(|| {
-            runner_with_fibonacci_tasks(black_box(1000), black_box(1_000_000), black_box(1000))
-        })
+        b.iter_with_setup(
+            || build_runner(1_000, 1_000_000),
+            |mut runner| run(black_box(&mut runner), black_box(1_000), black_box(1_000)),
+        )
     });
     c.bench_function("runner_with_fibonacci_tasks 10k🎯 1M🧠 1k➰", |b| {
-        b.iter(|| {
-            runner_with_fibonacci_tasks(black_box(10000), black_box(1_000_000), black_box(1000))
-        })
+        b.iter_with_setup(
+            || build_runner(10_000, 1_000_000),
+            |mut runner| run(black_box(&mut runner), black_box(10_000), black_box(1_000)),
+        )
     });
 
     c.bench_function("runner_with_fibonacci_tasks 8🎯 1G🧠 1k➰", |b| {
-        b.iter(|| {
-            runner_with_fibonacci_tasks(black_box(8), black_box(1_000_000_000), black_box(1000))
-        })
+        b.iter_with_setup(
+            || build_runner(8, 1_000_000_000),
+            |mut runner| run(black_box(&mut runner), black_box(8), black_box(1_000)),
+        )
     });
     c.bench_function("runner_with_fibonacci_tasks 1k🎯 1G🧠 1k➰", |b| {
-        b.iter(|| {
-            runner_with_fibonacci_tasks(black_box(1000), black_box(1_000_000_000), black_box(1000))
-        })
+        b.iter_with_setup(
+            || build_runner(1_000, 1_000_000_000),
+            |mut runner| run(black_box(&mut runner), black_box(1_000), black_box(1_000)),
+        )
     });
     c.bench_function("runner_with_fibonacci_tasks 10k🎯 1G🧠 1k➰", |b| {
-        b.iter(|| {
-            runner_with_fibonacci_tasks(black_box(10000), black_box(1_000_000_000), black_box(1000))
-        })
+        b.iter_with_setup(
+            || build_runner(10_000, 1_000_000_000),
+            |mut runner| run(black_box(&mut runner), black_box(10_000), black_box(1_000)),
+        )
     });
 }
 
