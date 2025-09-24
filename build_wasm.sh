c561c506233908a6216ed70de4911d58bc84e4c5
Bastien Sevajol <contact@bux.fr>
2025-09-24 19:34:53 +0200

    chore: wasm compat

--- build_wasm.sh
+++ build_wasm.sh
@@ -4,5 +4,6 @@ set -euo pipefail
 arg=${1:-}
 arg=$(echo "$arg" | tr '[:upper:]' '[:lower:]')
 
-cargo build --target wasm32-unknown-unknown --release --bin wui
-wasm-bindgen --no-typescript --out-name wui --out-dir wasm/ --target web target/wasm32-unknown-unknown/release/wui.wasm
+export RUSTFLAGS='--cfg getrandom_backend="wasm_js"'
+cargo build --target wasm32-unknown-unknown --release --bin gui
+wasm-bindgen --no-typescript --out-name gui --out-dir wasm/ --target web target/wasm32-unknown-unknown/release/gui.wasm
